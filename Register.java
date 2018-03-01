388eb15b40f33cc7a7d616e5eeacaabae13d7fcb
csinye <csinye@gmail.com>
2018-03-01 17:16:45 +0530

    cdcdc

--- Register.java
+++ Register.java
@@ -4,8 +4,8 @@ import javax.servlet.http.*;
 import java.sql.*;
 
 public class Register extends HttpServlet {
-    
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
