4f7d0d3bfc2838e3f820f5fdcf6a3741e0dd53fb
csinye <csinye@gmail.com>
2018-03-01 15:33:34 +0530

    test34

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,39 +1,27 @@
+package com.jwt.servlet;
+
 import java.io.IOException;
 import java.io.PrintWriter;
- 
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-@WebServlet("/LoginServlet")
+
 public class LoginServlet extends HttpServlet {
- 
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-         
-        // read form fields
-        String username = request.getParameter("name");
-        String email = request.getParameter("email");
-         
-        System.out.println("username: " + name);
-        System.out.println("email: " + email);
- 
-        // do some processing here...
-         
-        // get response writer
-        PrintWriter writer = response.getWriter();
-         
-        // build HTML code
-        String htmlRespone = "<html>";
-        htmlRespone += "<h2>Your username is: " + name + "<br/>";      
-        htmlRespone += "Your password is: " + email + "</h2>";    
-        htmlRespone += "</html>";
-         
-        // return response
-        writer.println(htmlRespone);
-         
-    }
- 
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String customer_name = request.getParameter("name"); 
+        String email = request.getParameter("email"); 
+        String course = request.getParameter("course"); 
+        String phno = request.getParameter("pno"); 
+        String amount = request.getParameter("amount"); 
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello " + "  " + name + "welcome to my blog");
+		out.println("Your password is : " + "  " + email + "<br>");
+		out.println("</body></html>");
+	}
 }
